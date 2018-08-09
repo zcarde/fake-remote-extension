380d2f6a10642c542338ecb27ce5f648f28cba08
zcarde <zcarde@student.42.fr>
2018-08-09 23:48:13 +0200

    ADD key positions

--- touches.js
+++ touches.js
@@ -11,51 +11,221 @@ let TOUCHES = {
         width: 61,
         height: 36
     },
-    LEFT: { y: 0, x: 0, width: 37, height: 25 },
-    RIGHT: { y: 0, x: 0, width: 37, height: 25 },
-    OK: { y: 0, x: 0, width: 37, height: 25 },
-    BACK: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_0: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_1: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_2: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_3: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_4: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_5: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_6: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_7: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_8: { y: 0, x: 0, width: 37, height: 25 },
-    NUM_9: { y: 0, x: 0, width: 37, height: 25 },
-    PLAY: { y: 0, x: 0, width: 37, height: 25 },
-    FORWARD: { y: 0, x: 0, width: 37, height: 25 },
-    REWIND: { y: 0, x: 0, width: 37, height: 25 },
-    STOP: { y: 0, x: 0, width: 37, height: 25 },
-    PAUSE: { y: 0, x: 0, width: 37, height: 25 },
-    PLAY_PAUSE: { y: 0, x: 0, width: 37, height: 25 },
-    HOME: { y: 0, x: 0, width: 37, height: 25 },
-    MENU: { y: 0, x: 0, width: 37, height: 25 },
-    APPS: { y: 0, x: 0, width: 37, height: 25 },
-    EXIT: { y: 0, x: 0, width: 37, height: 25 },
-    INFO: { y: 0, x: 0, width: 37, height: 25 },
-    HELP: { y: 0, x: 0, width: 37, height: 25 },
-    PVR: { y: 0, x: 0, width: 37, height: 25 },
-    REC: { y: 0, x: 0, width: 37, height: 25 },
-    FAVORITES: { y: 0, x: 0, width: 37, height: 25 },
-    SEARCH: { y: 0, x: 0, width: 37, height: 25 },
-    VOD: { y: 0, x: 0, width: 37, height: 25 },
-    AUDIO: { y: 0, x: 0, width: 37, height: 25 },
-    SUBTITLES: { y: 0, x: 0, width: 37, height: 25 },
-    GUIDE: { y: 0, x: 0, width: 37, height: 25 },
-    DEL: { y: 0, x: 0, width: 37, height: 25 },
-    FCT_1: { y: 0, x: 0, width: 37, height: 25 },
-    FCT_2: { y: 0, x: 0, width: 37, height: 25 },
-    FCT_3: { y: 0, x: 0, width: 37, height: 25 },
-    FCT_4: { y: 0, x: 0, width: 37, height: 25 },
-    PROG_UP: { y: 0, x: 0, width: 37, height: 25 },
-    PROG_DOWN: { y: 0, x: 0, width: 37, height: 25 },
-    VOL_UP: { y: 428, x: 25, width: 37, height: 34 },
-    VOL_DOWN: { y: 480, x: 25, width: 37, height: 34 },
-    MUTE: { y: 0, x: 0, width: 37, height: 25 },
-    POWER: { y: 0, x: 0, width: 37, height: 25 },
-    PREV: { y: 0, x: 0, width: 37, height: 25 },
-    NEXT: { y: 0, x: 0, width: 37, height: 25 },
+    LEFT: {
+        x: 36,
+        y: 291,
+        width: 36,
+        height: 61
+
+    },
+    RIGHT: {
+        x: 126,
+        y: 294,
+        width: 36,
+        height: 61
+    },
+    OK: {
+        x: 80,
+        y: 304,
+        width: 38,
+        height: 37
+    },
+    BACK: {
+        x: 24,
+        y: 391,
+        width: 42,
+        height: 25
+    },
+    NUM_0: {
+        x: 77,
+        y: 189,
+        width: 40,
+        height: 25
+    },
+    NUM_1: {
+        x: 24,
+        y: 77,
+        width: 40,
+        height: 25
+    },
+    NUM_2: {
+        x: 78,
+        y: 77,
+        width: 40,
+        height: 25
+    },
+    NUM_3: {
+        x: 132,
+        y: 77,
+        width: 40,
+        height: 25
+    },
+    NUM_4: {
+        x: 24,
+        y: 113,
+        width: 40,
+        height: 25
+    },
+    NUM_5: {
+        x: 79,
+        y: 113,
+        width: 40,
+        height: 25
+    },
+    NUM_6: {
+        x: 132,
+        y: 113,
+        width: 40,
+        height: 25
+    },
+    NUM_7: {
+        x: 24,
+        y: 152,
+        width: 40,
+        height: 25
+    },
+    NUM_8: {
+        x: 79,
+        y: 152,
+        width: 40,
+        height: 25
+    },
+    NUM_9: {
+        x: 132,
+        y: 152,
+        width: 40,
+        height: 25
+    },
+    FORWARD: {
+        x: 145,
+        y: 603,
+        width: 37,
+        height: 25
+    },
+    REWIND: {
+        x: 15,
+        y: 603,
+        width: 37,
+        height: 25
+    },
+    STOP: {
+        x: 58,
+        y: 603,
+        width: 37,
+        height: 25
+    },
+    PLAY_PAUSE: {
+        x: 101,
+        y: 603,
+        width: 37,
+        height: 25
+    },
+    MENU: {
+        x: 24,
+        y: 533,
+        width: 42,
+        height: 25
+    },
+    EXIT: {
+        x: 133,
+        y: 391,
+        width: 40,
+        height: 25
+    },
+    INFO: {
+        x: 78,
+        y: 411,
+        width: 42,
+        height: 25
+    },
+    REC: {
+        x: 58,
+        y: 636,
+        width: 37,
+        height: 25
+    },
+    VOD: {
+        x: 24,
+        y: 227,
+        width: 40,
+        height: 25
+    },
+    GUIDE: {
+        x: 133,
+        y: 227,
+        width: 40,
+        height: 25
+    },
+    FCT_1: {
+        x: 15,
+        y: 569,
+        width: 38,
+        height: 25
+    },
+    FCT_2: {
+        x: 58,
+        y: 569,
+        width: 38,
+        height: 25
+    },
+    FCT_3: {
+        x: 101,
+        y: 569,
+        width: 38,
+        height: 25
+    },
+    FCT_4: {
+        x: 145,
+        y: 569,
+        width: 38,
+        height: 25
+    },
+    PROG_UP: {
+        x: 139,
+        y: 429,
+        width: 36,
+        height: 38
+    },
+    PROG_DOWN: {
+        x: 139,
+        y: 479,
+        width: 36,
+        height: 38
+    },
+    VOL_UP: {
+        x: 25,
+        y: 429,
+        width: 36,
+        height: 38
+    },
+    VOL_DOWN: {
+        x: 25,
+        y: 479,
+        width: 36,
+        height: 38
+    },
+    MUTE: {
+        x: 24,
+        y: 41,
+        width: 40,
+        height: 25
+    },
+    POWER: {
+        x: 132,
+        y: 41,
+        width: 40,
+        height: 25
+    },
+    PREV: {
+        x: 87,
+        y: 489,
+        width: 25,
+        height: 25
+    },
+    NEXT: {
+        x: 87,
+        y: 452,
+        width: 25,
+        height: 25
+    }
 };
